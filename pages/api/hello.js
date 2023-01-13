44e01980922aa730d859d072e1fb1944ea6e92af
gisraeli <israeligal2@gmail.com>
2023-01-13 14:26:41 +0200

    feat:demo

---
+++ pages/api/hello.js
@@ -0,0 +1,5 @@
+//A Good Use Case: Handling Form Input
+
+export default function handler(req, res) {
+  res.status(200).json({ text: 'Hello' });
+}
---
+++ pages/index.js
@@ -0,0 +1,67 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Date from '../components/date';
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                    <Date dateString={date} />
+                </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  // won't be included in the JS bundle for client side
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// export async function getServerSideProps(context) {
+  // context include request params
+//   return {
+//     props: {
+//       // props for your component
+//     },
+//   };
+// }
+
+// import useSWR from 'swr';
+// https://swr.vercel.app/ enhanced fetch
+// function Profile() {
+//   const { data, error } = useSWR('/api/user', fetch);
+
+//   if (error) return <div>failed to load</div>;
+//   if (!data) return <div>loading...</div>;
+//   return <div>hello {data.name}!</div>;
+// }
